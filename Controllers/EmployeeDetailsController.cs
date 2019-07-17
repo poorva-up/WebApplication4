cc2995988de00dcd8966cc9ca5625d161070ef65
Poorva Pandav <poorva.pandav@gmail.com>
2019-07-17 11:10:00 +0530

    Initial commit

---
+++ Controllers/EmployeeDetailsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class EmployeeDetailsController : Controller
+    {
+        // GET: EmployeeDetails
+        public ActionResult Index()
+        {
+            List<EmployeeDetail> employees = new List<EmployeeDetail>();
+            employees = GetEmployeeDetails();
+            return View(employees);
+        }
+        public List<EmployeeDetail> GetEmployeeDetails()
+        {
+            using (EmployeeEntities1 employee = new EmployeeEntities1())
+            {
+                List<EmployeeDetail> employeeDetails = employee.EmployeeDetails.ToList();
+                return employeeDetails;
+
+            }
+        }
+
+        public ActionResult DisplayEmployeeDetails(EmployeeDetail emp)
+        {
+            return View("UpdateEmployee", emp);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateEmployee(EmployeeDetail emp)
+        {
+           
+            using (EmployeeEntities1 employee = new EmployeeEntities1())
+            {
+                if (ModelState.IsValid && emp!=null)
+                {
+                    var em = employee.Entry(emp);
+                    EmployeeDetail empObj = employee.EmployeeDetails.Where(x => x.EmployeeID == emp.EmployeeID).FirstOrDefault();
+                    empObj.EmployeeName = emp.EmployeeName;
+                    empObj.EmployeeContact = emp.EmployeeContact;
+                    empObj.EmployeeCountry = emp.EmployeeCountry;
+                    employee.SaveChanges();
+                    ViewBag.Message = "Changes Saved!";
+                    
+                }
+                else
+                {
+                    ViewBag.Message= "changes not saved!";
+                }
+                return View();
+
+            } 
+        }
+               
+    }
+}
---
+++ Controllers/UpdateEmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class UpdateEmployeeController : Controller
+    {
+        // GET: UpdateEmployee
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
